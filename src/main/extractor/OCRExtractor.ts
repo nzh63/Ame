093ac293348f06615ce0a3117f2c2054ca0fc2af
nzh63 <nzh21@outlook.com>
2021-04-14 19:24:41 +0800

    feat(ocr): 实现ScreenCapturer

--- src/main/extractor/OCRExtractor.ts
+++ src/main/extractor/OCRExtractor.ts
@@ -1,5 +1,10 @@
+import type { DModel as M } from 'win32-def';
 import type { Extractor } from '@main/extractor';
+import { gdi32, user32 } from '@main/win32';
 import EventEmitter from 'events';
+import fs from 'fs';
+import { nativeImage } from 'electron';
+import logger from '@logger/extractor/OCRExtractor';
 
 export class OCRExtractor extends EventEmitter implements Extractor {
     constructor() {
@@ -13,5 +18,60 @@ export class OCRExtractor extends EventEmitter implements Extractor {
 
     public destroy() {
         throw new Error('Not implemented.');
+    }
+}
+
+export class ScreenCapturer {
+    private hwnd?: M.HWND;
+    constructor(
+        public gamePids: number[],
+        hwnd?: M.HWND
+    ) {
+        if (hwnd) this.hwnd = hwnd;
+        else this.findWindow();
+        logger(this.hwnd);
+    }
+
+    private findWindow() {
+        const ret = user32.EnumWindows(hwnd => {
+            const lpdwProcessId = Buffer.alloc(4, 0);
+            user32.GetWindowThreadProcessId(hwnd, lpdwProcessId);
+            const pid = lpdwProcessId.readUInt32LE(0);
+            if (user32.IsWindowEnabled(hwnd) && user32.IsWindowVisible(hwnd) && this.gamePids.includes(pid)) {
+                this.hwnd = hwnd;
+                return 0;
+            } else {
+                return 1;
+            }
+        }, 0);
+        delete ret.callbackEntry;
+    }
+
+    public capture() {
+        if (!this.hwnd) throw new Error('window not found');
+
+        const rect = Buffer.alloc(4 * 4, 0);
+        user32.GetWindowRect(this.hwnd, rect);
+        const left = rect.readInt32LE(0);
+        const top = rect.readInt32LE(4);
+        const right = rect.readInt32LE(8);
+        const bottom = rect.readInt32LE(12);
+        const width = right - left;
+        const height = bottom - top;
+
+        const hwndDC = user32.GetWindowDC(this.hwnd);
+        const saveDC = gdi32.CreateCompatibleDC(hwndDC);
+        const saveBitmap = gdi32.CreateCompatibleBitmap(hwndDC, width, height);
+        gdi32.SelectObject(saveDC, saveBitmap);
+        user32.PrintWindow(this.hwnd, saveDC, 0x00000001 | 0x00000002 /* PW_CLIENTONLY | PW_RENDERFULLCONTENT */);
+        const buf = Buffer.alloc(width * height * 4, 0);
+        gdi32.GetBitmapBits(saveBitmap, width * height * 4, buf);
+        gdi32.DeleteObject(saveBitmap);
+        user32.ReleaseDC(this.hwnd, hwndDC);
+        gdi32.DeleteObject(saveBitmap);
+
+        const img = nativeImage.createFromBitmap(buf, { width, height });
+        fs.writeFile('./a.png', img.toPNG(), () => { });
+        return img;
     }
 }
---
+++ src/main/hook/GlobalKeyboardEventHook.ts
@@ -0,0 +1,51 @@
+import EventEmitter from 'events';
+import { user32 } from '@main/win32';
+import logger from '@logger/hook/globalKeyboardEventHook';
+
+export class GlobalKeyboardEventHook {
+    private keyboardEventHookHandle?: ReturnType<typeof user32.SetWinEventHook>;
+    constructor(
+        public event: EventEmitter
+    ) {
+        this.startHook();
+    }
+
+    private startHook() {
+        logger('start GlobalKeyboardEvent hook');
+        this.keyboardEventHookHandle = user32.SetWindowsHookExA(
+            13, /* WH_KEYBOARD_LL */
+            (nCode, wParam, lParam) => {
+                if (nCode >= 0) {
+                    const vkCode = lParam.readInt32LE(0);
+                    if (wParam === 0x0100 /* WM_KEYDOWN */) {
+                        logger('key-down %d', vkCode);
+                        setImmediate(() => this.event.emit('key-down', vkCode));
+                    } else if (wParam === 0x0101 /* WM_KEYUP */) {
+                        logger('key-up %d', vkCode);
+                        setImmediate(() => this.event.emit('key-up', vkCode));
+                    }
+                }
+                return user32.CallNextHookEx(0, nCode, wParam, lParam);
+            },
+            0,
+            0
+        );
+        if (this.keyboardEventHookHandle.nativeReturnValue) {
+            logger('hook success');
+        } else {
+            logger('hook fail');
+        }
+    }
+
+    private stopHook() {
+        logger('stop GlobalKeyboardEvent hook');
+        if (this.keyboardEventHookHandle) {
+            user32.UnhookWindowsHookEx(this.keyboardEventHookHandle);
+        }
+        this.keyboardEventHookHandle = undefined;
+    }
+
+    public destroy() {
+        this.stopHook();
+    }
+}
