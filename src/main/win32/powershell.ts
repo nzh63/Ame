7126f3ccce63a46722eb18774b48a524ea1c514e
nzh63 <nzh21@outlook.com>
2021-04-15 12:43:57 +0800

    fix: 处理无法运行powershell时的情况

--- src/main/win32/powershell.ts
+++ src/main/win32/powershell.ts
@@ -6,15 +6,18 @@ import { promisify } from 'util';
 const execPromisify = promisify(exec);
 
 let powershellPath = '"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"';
-let powershellReadyResolve: (value: void | PromiseLike<void>) => void;
-const powershellReady = new Promise<void>(resolve => {
+let powershellReadyResolve: () => void;
+let powershellReadyReject: (e: unknown) => void;
+const powershellReady = new Promise<void>((resolve, reject) => {
     powershellReadyResolve = resolve;
+    powershellReadyReject = reject;
 });
 fs.stat(powershellPath, async (err) => {
     if (err) powershellPath = 'powershell.exe';
     try {
         await execPromisify(`${powershellPath} /?`);
     } catch (e) {
+        powershellReadyReject(e);
         dialog.showErrorBox('环境出错', `无法运行Powershell\n${e.message ?? e}`);
         app.exit(1);
     }
