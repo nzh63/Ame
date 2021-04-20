a7993432a2fab6bbee215da6d098b2ab807e7c46
nzh63 <nzh21@outlook.com>
2021-04-20 17:49:33 +0800

    fix(Textractor): 修复多行文本时文本缺失的问题

--- src/main/extractor/Textractor/index.ts
+++ src/main/extractor/Textractor/index.ts
@@ -71,10 +71,10 @@ export class Textractor extends BaseExtractor {
         this.textractorCliProcess.stdout.on('data', data => {
             data = '' + data;
             this.textractorCliStdoutBuffer += data;
-            const result = this.textractorCliStdoutBuffer.split('\n');
+            const result = this.textractorCliStdoutBuffer.split(/\n(?=\[)/);
             while (result.length > 1) {
                 const line = '' + result.shift();
-                const [, key, text] = /\[(.*?)\](.*)/.exec(line) ?? [null, null, null];
+                const [, key, text] = /^\[(.*?)\]([\S\s]*)$/.exec(line) ?? [null, null, null];
                 if (key && text) {
                     this.onUpdate(key, text);
                 }
