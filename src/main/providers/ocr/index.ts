94154c5d679110168c1f3eb59e175c1021ee8e8e
nzh63 <nzh21@outlook.com>
2021-04-27 14:46:36 +0800

    feat(ocr): 添加腾讯云作为OCR识别

--- src/main/providers/ocr/index.ts
+++ src/main/providers/ocr/index.ts
@@ -1,11 +1,12 @@
 import type { Schema } from '@main/schema';
 import type { OcrProviderOptions, OcrProviderMethods, OcrProviderConfig } from '@main/providers/OcrProvider';
 import tesseract from './tesseract';
+import tencentcloud from './tencentcloud';
 import baiduAi from './baiduAi';
 
 export function defineOcrProvider<ID extends string, S extends Schema, D>(arg: OcrProviderOptions<ID, S, D>, methods: OcrProviderMethods<ID, S, D>) {
     return { ...arg, ...methods, providersStoreKey: 'ocrProviders' } as OcrProviderConfig<ID, S, D>;
 }
 
-export const availableOcrConfigs = [tesseract, baiduAi] as const;
+export const availableOcrConfigs = [tesseract, tencentcloud, baiduAi] as const;
 export type AvailableOcrConfigs = typeof availableOcrConfigs;
---
+++ src/main/providers/ocr/tencentcloud.ts
@@ -0,0 +1,90 @@
+import { defineOcrProvider } from '@main/providers/ocr';
+import { Client } from 'tencentcloud-sdk-nodejs/tencentcloud/services/ocr/v20181119/ocr_client';
+
+export default defineOcrProvider({
+    id: '腾讯云',
+    description: '腾讯云通用印刷体识别API，详见 https://cloud.tencent.com/product/generalocr \n会向 ocr.tencentcloudapi.com 发送数据',
+    optionsSchema: {
+        enable: Boolean,
+        apiConfig: {
+            credential: {
+                secretId: [String, null],
+                secretKey: [String, null]
+            },
+            region: String,
+            params: {
+                LanguageType: {
+                    type: String,
+                    enum: [
+                        'auto', 'zh', 'jap', 'kor', ' spa', 'fre', 'ger',
+                        'por', ' vie', 'may', 'rus', 'ita', ' hol', 'swe',
+                        'fin', 'dan', ' nor', 'hun', 'tha', 'lat', 'ara'
+                    ]
+                }
+            }
+        }
+    } as const,
+    defaultOptions: {
+        enable: true,
+        apiConfig: {
+            credential: {
+                secretId: null,
+                secretKey: null
+            },
+            region: 'ap-guangzhou',
+            params: {
+                LanguageType: 'jap',
+            }
+        }
+    },
+    optionsDescription: {
+        enable: '启用',
+        apiConfig: {
+            credential: {
+                secretId: { readableName: '密钥ID', description: '可以在 https://console.cloud.tencent.com/cam/capi 获取' },
+                secretKey: { readableName: '密钥KEY', description: '可以在 https://console.cloud.tencent.com/cam/capi 获取' }
+            },
+            region: '地域',
+            params: {
+                LanguageType: '语言'
+            }
+        }
+    },
+    data() {
+        return {
+            client: null as null | Client
+        };
+    }
+}, {
+    init() {
+        if (!this.options.apiConfig.credential.secretId || !this.options.apiConfig.credential.secretKey) return;
+        const clientConfig = {
+            credential: {
+                secretId: this.options.apiConfig.credential.secretId,
+                secretKey: this.options.apiConfig.credential.secretKey
+            },
+            region: 'ap-guangzhou',
+            profile: {
+                httpProfile: {
+                    endpoint: 'ocr.tencentcloudapi.com'
+                }
+            }
+        };
+        this.data.client = new Client(clientConfig);
+    },
+    isReady() {
+        return this.options.enable &&
+            this.options.apiConfig.credential.secretId !== null &&
+            this.options.apiConfig.credential.secretKey !== null &&
+            !!this.data.client;
+    },
+    async recognize(img) {
+        if (!this.data.client) throw new Error('client have not been init');
+        const params = { ...this.options.apiConfig.params, ImageBase64: Buffer.from(await img.clone().png().toBuffer()).toString('base64') };
+        const res = await this.data.client.GeneralBasicOCR(params);
+        if (res.TextDetections) {
+            return res.TextDetections.map(i => i.DetectedText).join('');
+        }
+        throw res;
+    }
+});
