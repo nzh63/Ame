96492728599754bb5fc2006a105ac8944f1c7c8c
nzh63 <nzh21@outlook.com>
2021-05-10 00:13:41 +0800

    refactor(logger): 移除无用代码

--- src/main/logger/index.ts
+++ src/main/logger/index.ts
@@ -1,5 +1,4 @@
 import debug from 'debug';
-debug.enable('ame:main:*');
 if (import.meta.env.DEV) {
     debug.enable('ame:main,ame:main:*,ame:render,ame:render:*');
     debug.log = console.log.bind(console);
