de08edc8d2ecabf5218d682a3a548c2efdcb68f6
nzh63 <nzh21@outlook.com>
2021-08-04 16:19:32 +0800

    fix(render): 修复编辑和无法启动

--- src/render/remote/startGame.ts
+++ src/render/remote/startGame.ts
@@ -2,6 +2,8 @@ import { handleError } from '@render/remote/handle';
 
 const electron = require('electron');
 
+function t<T>(o: T): T { return JSON.parse(JSON.stringify(o)); }
+
 export function startGame(game: Ame.GameSetting) {
-    return handleError(electron.ipcRenderer.invoke('start-game', game));
+    return handleError(electron.ipcRenderer.invoke('start-game', t(game)));
 }
