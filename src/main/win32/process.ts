ab693c10ca5478056d8a0372a830508949c08b66
nzh63 <nzh21@outlook.com>
2021-08-14 19:38:28 +0800

    fix(shell): 修复命令行转义

--- src/main/win32/process.ts
+++ src/main/win32/process.ts
@@ -3,7 +3,7 @@ import { execPowerShell } from '@main/win32';
 export { waitProcessForExit, isWow64 } from '@addons/Process';
 
 export async function findProcess(path: string) {
-    const { stdout } = await execPowerShell(`Get-Process | where ProcessName -eq '${parse(path).name}' | where Path -eq '${path}' | sort StartTime | select id`);
+    const { stdout } = await execPowerShell(`Get-Process | where ProcessName -eq '${parse(path).name.replace(/'/g, "''")}' | where Path -eq '${path.replace(/'/g, "''")}' | sort StartTime | select id`);
     return stdout
         .split('\n')
         .map(i => i.trim())
