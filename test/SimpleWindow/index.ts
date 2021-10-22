45861baa17c736b0b6913219191e2d30abc1bc57
nzh63 <nzh21@outlook.com>
2021-10-22 22:12:15 +0800

    revert: test: 提高测试稳定性
    This reverts commit 35276a24617eaa886eae10b1137747b408a87dcf.

--- test/SimpleWindow/index.ts
+++ test/SimpleWindow/index.ts
@@ -1,73 +1,44 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import EventEmitter from 'events';
 import http from 'http';
 import path from 'path';
-import { spawn, ChildProcess } from 'child_process';
 import { execPowerShell, findProcess } from '@main/win32';
 import fetch from 'electron-fetch';
 
 const electronPath = path.join(process.cwd(), './node_modules/electron/dist/electron.exe');
 
 export class SimpleWindow extends EventEmitter {
-    private server?: http.Server;
-    private child?: ChildProcess;
+    private server!: http.Server;
     private childPort!: number;
     private readyPromise: Promise<void>;
     public pids: number[] = [];
 
     constructor() {
         super();
-        // eslint-disable-next-line no-async-promise-executor
-        this.readyPromise = new Promise<void>(async resolve => {
-            for (let i = 1; i <= 3; i++) {
-                await Promise.race([
-                    this.startChild(),
-                    new Promise(resolve => setTimeout(resolve, i * 3 * 1000))
-                ]);
-                if (this.pids.length) {
-                    resolve();
-                    break;
+        let readyPromiseResolve: () => void;
+        this.readyPromise = new Promise<void>(resolve => { readyPromiseResolve = resolve; });
+        (async () => {
+            const oldPids = await findProcess(electronPath);
+            this.server = http.createServer(async (req, resp) => {
+                if (req.socket.remoteAddress === '127.0.0.1') {
+                    let msg = '';
+                    req.on('data', data => { msg += '' + data; });
+                    await new Promise(resolve => req.once('end', resolve));
+                    this.childPort = parseInt(msg);
+                    resp.end();
+                    this.server.close();
+                    this.server.unref();
+                    this.pids = (await findProcess(electronPath)).filter(i => !oldPids.includes(i));
+                    this.emit('ready');
+                    readyPromiseResolve!();
                 } else {
-                    this.closeServer();
+                    resp.end();
                 }
-            }
-        });
-    }
-
-    private startChild() {
-        this.child?.kill();
-        this.child = undefined;
-        return new Promise<void>(resolve => {
-            findProcess(electronPath)
-                .then(oldPids => {
-                    this.closeServer();
-                    this.server = http.createServer(async (req, resp) => {
-                        if (req.socket.remoteAddress === '127.0.0.1') {
-                            let msg = '';
-                            req.on('data', data => { msg += '' + data; });
-                            await new Promise(resolve => req.once('end', resolve));
-                            this.childPort = parseInt(msg);
-                            resp.end();
-                            this.closeServer();
-                            this.pids = (await findProcess(electronPath)).filter(i => !oldPids.includes(i));
-                            this.emit('ready');
-                            resolve();
-                        } else {
-                            resp.end();
-                        }
-                    });
-                    this.server.listen(0, '127.0.0.1');
-                })
-                .then(() => new Promise(resolve => this.server?.once('listening', resolve)))
-                .then(() => {
-                    this.child = spawn(electronPath, ['./test/SimpleWindow/run.js', '' + (this.server?.address() as any).port], { cwd: path.join(__dirname, '../..') });
-                });
-        });
-    }
-
-    private closeServer() {
-        this.server?.close();
-        this.server?.unref();
-        this.server = undefined;
+            });
+            this.server.listen(0, '127.0.0.1');
+            await new Promise(resolve => this.server.once('listening', resolve));
+            execPowerShell(`&'${electronPath}' ./test/SimpleWindow/run.js ${(this.server.address() as any).port}`);
+        })();
     }
 
     public whenReady() {
@@ -82,6 +53,6 @@ export class SimpleWindow extends EventEmitter {
         try {
             await execPowerShell(`taskkill /f /pid ${this.pids.join(' /pid ')}`);
         } catch (e) { }
-        this.server?.unref();
+        this.server.unref();
     }
 }
