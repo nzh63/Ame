19f522096245e84cc42dabbc3ed09817d1193d9f
nzh63 <nzh21@outlook.com>
2021-10-24 16:22:02 +0800

    refactor(ocr):  移除不推荐使用的win32 api调用

--- src/main/extractor/OcrExtractor/ScreenCapturer.ts
+++ src/main/extractor/OcrExtractor/ScreenCapturer.ts
@@ -29,19 +29,11 @@ export class ScreenCapturer {
         }
         const { width, height, buffer } = result;
 
-        const img = sharp(buffer, { raw: { width, height, channels: 4 } });
-        const [rBuffer, gBuffer, bBuffer, aBuffer] = await Promise.all([
-            img.clone().extractChannel(2).raw().toColorspace('b-w').toBuffer(),
-            img.clone().extractChannel(1).raw().toColorspace('b-w').toBuffer(),
-            img.clone().extractChannel(0).raw().toColorspace('b-w').toBuffer(),
-            img.clone().extractChannel(3).raw().toColorspace('b-w').toBuffer()
-        ]);
-        const r = sharp(rBuffer, { raw: { width, height, channels: 1 } });
-        const rgba = r
-            .joinChannel(gBuffer, { raw: { width, height, channels: 1 } })
-            .joinChannel(bBuffer, { raw: { width, height, channels: 1 } })
-            .joinChannel(aBuffer, { raw: { width, height, channels: 1 } });
-        return sharp(await rgba.raw().toBuffer(), { raw: { width, height, channels: 4 } });
+        const img = sharp(buffer, { raw: { width, height, channels: 4 } })
+            .recomb([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
+            .flip();
+
+        return img;
     }
 
     public destroy() { }
