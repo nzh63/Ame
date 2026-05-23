06f6a0ec22444539e495575c1018892b245e031a
nzh63 <nzh21@outlook.com>
2026-05-23 19:49:57 +0800

    test: 在ia32架构下跳过tesseract测试

--- test/main/providers/ocr/tesseract.spec.ts
+++ test/main/providers/ocr/tesseract.spec.ts
@@ -2,7 +2,11 @@ import { buildTest } from '.';
 import '../../env';
 import tesseract from '@main/providers/ocr/tesseract';
 
-buildTest(tesseract, {
-  enable: true,
-  language: 'jpn',
-});
+buildTest(
+  tesseract,
+  {
+    enable: true,
+    language: 'jpn',
+  },
+  process.arch === 'ia32',
+);
